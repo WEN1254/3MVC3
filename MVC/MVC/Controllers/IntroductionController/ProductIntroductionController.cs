7f491e382e2b2c97d846539fe2bea938b0bc0841
Hank <h50614ank@gmail.com>
2020-06-20 19:33:32 +0800

    Introduction變更

---
+++ MVC/MVC/Controllers/IntroductionController/ProductIntroductionController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers.IntroductionController
+{
+    public class ProductIntroductionController : Controller
+    {
+
+        //Sofas(current)
+        //Chairs(current)
+        //Tables(current)
+        //Beds(current)
+        //Cabinet And Dressingtable(current)
+        //Accessories
+        // GET: ProductIntroduction
+        public ActionResult Sofas()
+        {
+            return View();
+        }
+        public ActionResult Chairs()
+        {
+            return View();
+        }
+        public ActionResult Tables()
+        {
+            return View();
+        }
+        public ActionResult Beds()
+        {
+            return View();
+        }
+        public ActionResult CabinetAndDressingtable()
+        {
+            return View();
+        }
+        public ActionResult Accessories()
+        {
+            return View();
+        }
+    }
+}
